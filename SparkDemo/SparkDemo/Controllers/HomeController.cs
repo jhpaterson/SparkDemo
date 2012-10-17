0413794e2ba20e77c425662c068d8726f954a050
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-10-17 23:24:23 +0100

    initial commit

---
+++ SparkDemo/SparkDemo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SparkDemo.Models;
+
+namespace SparkDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Products = new List<Product>{
+                new Product{ProductID=1,ProductName="Banana",Description="Yellow",Price=9.99M},
+                new Product{ProductID=2,ProductName="Apple",Description="Green",Price=9.99M},
+                new Product{ProductID=3,ProductName="Orange",Description="Blue",Price=9.99M}
+            };
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ SparkDemo/SparkDemo/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparkDemo.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
